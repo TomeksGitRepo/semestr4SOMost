9a6bcd52649b7eefb5f0eb93d358bb7784544d0e
Tomek <tomek7sa@gmail.com>
2019-06-07 10:48:38 +0200

    Implemented all methods

--- most.cpp
+++ most.cpp
@@ -5,6 +5,8 @@
 #include <mutex>
 #include <vector>
 #include <unistd.h>
+#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
@@ -18,8 +20,8 @@ class Miasto {
 
 
 	public:
-	void zwieksz_ilosc();
-	void zmniejsz_ilosc();
+	void zwieksz_ilosc(int nr_samochodu);
+	void zmniejsz_ilosc(int nr_samochodu);
 	int podaj_ilosc_samochodow();
 	void dodaj_samochod_do_kolejki(int do_dodania);
 	void dodaj_do_przejazdzki(int do_dodania);
@@ -34,22 +36,42 @@ class Miasto {
 class Most {
 	public:
 	int nr_samochodu = 0; //0 means most is empty
-	string z_miasta;
 	Miasto* obecne_miasto;
 	void set_nr_samochodu(int nr_samochodu);
 	int get_nr_przejezdzajacego();
-	void set_z_miasta(string miasto_start);
-
+	void set_z_miasta(Miasto* miasto_start);
+	void zaimij_most();
 	private:
 	mutex mtx;
 };
 
+void Most::zaimij_most() {
+	mtx.lock();
+	sleep(5);
+	mtx.unlock();
 
+}
+
+void Most::set_nr_samochodu(int nr_samochodu) {
+	mtx.lock();
+	this->nr_samochodu = nr_samochodu;
+	mtx.unlock();
+}
+
+int Most::get_nr_przejezdzajacego() {
+	return this->nr_samochodu;
+}
+
+void Most::set_z_miasta(Miasto* miasto_start) {
+	mtx.lock();
+	this->obecne_miasto = miasto_start;
+	mtx.unlock();
+}
 
 class Samochod {
 	public:
 	int numer;
-	string miasto;
+	Miasto* miasto;
 	void przejedz_przez_most();
 	void przejadzka_po_miescie();
 	void pojedz_do_kolejki();
@@ -59,8 +81,21 @@ class Samochod {
 };
 
 void Samochod::run() {
+	int losowa_licza = rand() % 2;
 	
+	while(1) {
+	if (losowa_licza == 0) {
+		this->przejadzka_po_miescie();
+	} else {
+		this-> pojedz_do_kolejki();
+	}
+	sleep(30);
+	}
 
+}
+
+void Samochod::pojedz_do_kolejki() {
+	this->obecne_miasto->dodaj_samochod_do_kolejki(this->numer);	
 }
 
 void Samochod::przejadzka_po_miescie() {
@@ -76,6 +111,7 @@ void Samochod::przejedz_przez_most() {
 	this->most_pointer->set_nr_samochodu(this->numer);
 	this->most_pointer->set_z_miasta(this->miasto);
 	sleep(3);
+	this->obecne_miasto->zmniejsz_ilosc(this->numer);
 	//TODO finish this
 }
 
@@ -93,12 +129,16 @@ void Miasto::dodaj_do_przejazdzki(int do_dodania) {
 	this->mtx.unlock();
 }
 
-void Miasto::zwieksz_ilosc() {
-	this->ilosc_samochodow++;
+void Miasto::zwieksz_ilosc(int nr_samochodu) {
+	this->mtx.lock();
+	this->samochody_w_miescie.push_back(nr_samochodu);
+	this->mtx.unlock();
 }
 
 void Miasto::zmniejsz_ilosc(int nr_samochodu) {
-	this->samochody_w_miescie.remove(nr_samochodu);
+	this->mtx.lock();
+	this->samochody_w_miescie.erase(std::remove(this->samochody_w_miescie.begin(), this->samochody_w_miescie.end(), nr_samochodu), this->samochody_w_miescie.end());
+	this->mtx.unlock();
 }
 
 int Miasto::podaj_ilosc_samochodow() {
@@ -107,12 +147,6 @@ int Miasto::podaj_ilosc_samochodow() {
 }
 
 int main() {
-	Miasto A;
-	A.nazwa = "A";
-	A.ilosc_samochodow = 10;
-
-	A.zwieksz_ilosc();
-	A.podaj_ilosc_samochodow();
 
 	pthread_t tid[10];
 
